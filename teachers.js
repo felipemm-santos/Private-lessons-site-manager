b26d59af1af9348ebc6953ca9a00cf971adbe821
Felipe Morais Mariano dos Santos <tfelipesantos99s@gmail.com>
2020-07-14 08:01:03 -0300

    Finish show function and add edit function

--- teachers.js
+++ teachers.js
@@ -1,6 +1,14 @@
 const fs = require('fs');
+const Intl = require('intl');
+
 const data = require('./data.json');
-const { getAge, getGraduation } = require('./utils');
+
+const {
+  getAge,
+  getGraduationText,
+  getClassTypeText,
+  dateFormat,
+} = require('./utils');
 
 exports.post = (req, res) => {
   const Keys = Object.keys(req.body);
@@ -57,12 +65,34 @@ exports.show = (req, res) => {
   const teacher = {
     ...foundTeacher,
     age: getAge(foundTeacher.birth),
-    schooling: getGraduation(foundTeacher.schooling),
+
+    schooling: getGraduationText(foundTeacher.schooling),
+
+    class_type: getClassTypeText(foundTeacher.class_type),
+
     subjects: foundTeacher.subjects.split(','),
-    created_at: new Intl.DateTimeFormat('en-GB').format(
+
+    created_at: new Intl.DateTimeFormat('pt-BR').format(
       foundTeacher.created_at
     ),
   };
 
   return res.render('teachers/show', { teacher });
+};
+
+exports.edit = (req, res) => {
+  const { id } = req.params;
+
+  const foundTeacher = data.teachers.find((teacher) => id == teacher.id);
+
+  if (!foundTeacher) {
+    return res.render('not-found');
+  }
+
+  const teacher = {
+    ...foundTeacher,
+    birth: dateFormat(foundTeacher.birth),
+  };
+
+  return res.render('teachers/edit.njk', { teacher });
 };
--- utils.js
+++ utils.js
@@ -1,8 +1,42 @@
 module.exports = {
   getAge: (timestamp) => {
-    return timestamp;
+    const today = new Date();
+    const birthDate = new Date(timestamp);
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+
+    const month = today.getMonth() - birthDate.getMonth();
+
+    const isNotBirthDay =
+      month < 0 || (month == 0 && today.getDate() < birthDate.getDate());
+
+    if (isNotBirthDay) {
+      age -= 1;
+    }
+
+    return age;
   },
-  getGraduation: (schooling) => {
-    
+
+  getGraduationText: (schooling) => {
+    switch (schooling) {
+      case 'high_school':
+        return 'Ensino Médio Completo';
+      case 'college':
+        return 'Ensino Superior Completo';
+      case 'master':
+        return 'Mestrado';
+      case 'doctorate':
+        return 'Doutorado';
+    }
   },
+
+  getClassTypeText: (classType) => {
+    if (classType == 'distanced') {
+      return 'Á distância';
+    } else {
+      return 'Presencial';
+    }
+  },
+
+  dateFormat: (timestamp) => {},
 };
